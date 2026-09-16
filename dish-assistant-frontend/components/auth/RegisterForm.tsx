cf93b77024db01c2094e74dababc301663cdbdc6
baileynance <baieyj.nance@gmail.com>
2026-09-16 14:28:36 -0500

    This is a rough skeleton of how the frontent side of the project will be organized. All files are filled with placeholder data to keep errors from occuring when testing on localhost. Theywill need to be updated over time. NextJS, Tailwind, and EsLint initialized as well

---
+++ dish-assistant-frontend/components/auth/RegisterForm.tsx
@@ -0,0 +1,7 @@
+export default function RegisterForm() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-8">
+      <h1 className="text-2xl font-semibold">RegisterForm</h1>
+    </main>
+  );
+}
